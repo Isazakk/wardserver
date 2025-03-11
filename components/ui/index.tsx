e366e845bb85155327a8d237e49788ebbb24bd4d
Isazakk <isazakk@gmail.com>
2025-03-11 17:41:57 -0500

    Add missing UI components to fix build errors

---
+++ components/ui/index.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+// Re-export all UI components
+export * from "./button"
+export * from "./card"
+export * from "./dialog"
+export * from "./input"
+export * from "./label"
+export * from "./progress"
+export * from "./radio-group"
+export * from "./select"
+export * from "./slider"
+export * from "./tabs"
+export * from "./textarea"
+export * from "./toast"
+export * from "./use-toast"
+export * from "./badge"
+export * from "./table" 
